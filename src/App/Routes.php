8ac2e77b5a222964355bdb37d3520274370bb2b4
Valentin Culasso <val_nic@hotmail.com.ar>
2024-09-18 10:14:54 -0300

    Agregue comentarios

--- src/App/Routes.php
+++ src/App/Routes.php
@@ -9,14 +9,15 @@ require __DIR__ . "/../Controllers/usuarioController.php";
 
     $app->get('/usuario/{id}',function(Request $request, Response $response){
 
+        // Instancio la clase
         $usuarioController = new usuarioController();
-
+        // Cargo en una variable user_id el id que me viene en el request
         $user_id = $request -> getAttribute('id');
-        
+        // Cargo en respuesta la tabla con los datos del usuario con id = user_id
         $respuesta = $usuarioController->getUser($user_id);
-
+        // Genero un json con los datos del usuario
         $response->getBody()->write(json_encode($respuesta['result']));
-
+        // Me retorna el status -> 200 OK (me trae el usuario "el cual existe") o 404 en caso de ser null (usuario inexistente)
         return $response->withHeader('Content-Type', 'application/json')->withStatus($respuesta['status']);
 
     });
--- src/Controllers/usuarioController.php
+++ src/Controllers/usuarioController.php
@@ -1,4 +1,4 @@
-<?php //namespace
+<?php
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
@@ -10,33 +10,37 @@ class usuarioController {
     public function getUser($id){
         try{
 
+            // Me conecto a la base de datos
             $conn = conectarbd();
-        
+            // En $sql genero la consulta SQL
             $sql = "SELECT * FROM `juego` WHERE id = $id";
-        
+            // Si no me equivoco envio la coneccion a la base de datos y la consulta y me guarda en $result el contenido del id $id
             $result = mysqli_query($conn, $sql);
-        
+            // Si no me equivoco me convierte el $result en un array con los datos del usuario
             $response = mysqli_fetch_array($result);
-
+            // Si $response es null entonces envio un status 404
             if(!$response){
 
                 $respuesta = ['status'=> 404, 'result'=>"ID del usuario inexistente"];
 
             }
+            // Si no $respuesta es valida y envio un status 200 OK y en result el array con los datos del usuario ($response)
             else{
 
                 $respuesta = ['status'=>200, 'result'=>$response];
 
             }
+            // Una vez terminado me desconecto de la base de datos
             $conn = desconectarbd($conn);
         }
         catch(Exception $e){
 
+            // Si por ejemplo no me pude conectar a la base de datos envio un status 500
             $respuesta = ['status'=>500, 'result'=> $e->getMessage()];
 
         }
 
-
+        // retorno $respuesta -> la cual puede ser un status 200 OK , 404 not found o 500
         return $respuesta;
 
     }
