59090dad460e17706109c602b3ec3e80f8de615f
Valentin Culasso <val_nic@hotmail.com.ar>
2024-09-22 20:45:10 -0300

    act

--- src/Controllers/usuarioController.php
+++ src/Controllers/usuarioController.php
@@ -3,7 +3,7 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
-require __DIR__ . "/../App/Functions.php";
+require_once __DIR__ . "/../App/Functions.php";
 
 class usuarioController {
 
